4e2a7fc1b1f26e3de5714b9370a8034ce42c4cea
karry3775 <kartikprakash3775@gmail.com>
2021-02-21 10:35:54 -0500

    Add separate creations of grid and maze type objects

--- PathFinding/src/main.cpp
+++ PathFinding/src/main.cpp
@@ -16,31 +16,44 @@ int main(int argc, char** argv) {
     uint32_t cell_size{20};
 
     // Choose a starting point
-    PF::Cell start(2, 2);
+    PF::Cell start(15, 12);
 
     // Choose an ending point
     PF::Cell end(39, 39);
 
-    // Create a grid PathFinderObject
+    // Create a grid PathFinder Object
     PF::PathFinder* grid_path_finder = new PF::PathFinder(
         PF::EnvironmentType::GRID, PF::PathFindingMethod::BFS,
         PF::ObstacleGenerationMethod::FISHER_YATES_SHUFFLE,
         /**num_obstacles**/ 300, rows, cols, cell_size, start, end);
 
-    PF::PathFinder* maze_path_finder = 
-    new PF::PathFinder(PF::EnvironmentType::MAZE, PF::PathFindingMethod::BFS, PF::MazeGenerationMethod::RANDOMIZED_DFS, rows, cols, cell_size, start, end);
-
-    /** Visualize the environment **/
     // Create a visualizer object for the grid object
-    PF::Visualizer* visualizer(new PF::Visualizer(maze_path_finder));
+    PF::Visualizer* grid_visualizer(new PF::Visualizer(grid_path_finder));
     // Set title
-    visualizer->setTitle("MAZE");
+    grid_visualizer->setTitle("GRID");
     // Set theme
-    visualizer->setTheme(PF::Theme::LIGHT);
+    grid_visualizer->setTheme(PF::Theme::LIGHT);
     // Initiate the grid
-    visualizer->init();
+    grid_visualizer->init();
     // Show
-    visualizer->show();
+    grid_visualizer->show();
+
+    // Create a maze PathFinder Object
+    PF::PathFinder* maze_path_finder = new PF::PathFinder(
+        PF::EnvironmentType::MAZE, PF::PathFindingMethod::BFS,
+        PF::MazeGenerationMethod::RANDOMIZED_DFS, rows, cols, cell_size, start,
+        end);
+
+    // Create a visualizer object for the maze object
+    PF::Visualizer* maze_visualizer(new PF::Visualizer(maze_path_finder));
+    // Set title
+    maze_visualizer->setTitle("MAZE");
+    // Set theme
+    maze_visualizer->setTheme(PF::Theme::LIGHT);
+    // Initiate the grid
+    maze_visualizer->init();
+    // Show
+    maze_visualizer->show();
 
     return 0;
 }
